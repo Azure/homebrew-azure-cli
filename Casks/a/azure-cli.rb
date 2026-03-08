c2492f2d5c2cfbf81ff51c501cf72783095019d2
Azure CLI Team <AzPyCLI@microsoft.com>
2026-03-08 23:44:57 +0000

    azure-cli 2.84.0

--- Casks/a/azure-cli.rb
+++ Casks/a/azure-cli.rb
@@ -3,10 +3,10 @@ cask "azure-cli" do
   os macos: "macos"
 
   version "2.84.0"
-  sha256 arm:   "73f643a7935134af382e77089304893a89e14455e2484840ba73fa69e9b04a06",
-         intel: "25c560f55d1d52027f89cfae24acdad0f066b08b09a07c3cb539e09c27007f29"
+  sha256 arm:   "be1832758464d2f22ef0f4efcc5ee4155d253f12ddeb244b2dcca913992df987",
+         intel: "862be75742e3dc2e92bee5ae68a7a67a4bd028ada5e1a1fdf92297fe822be62e"
 
-  url "https://github.com/Azure/homebrew-azure-cli/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
+  url "https://github.com/naga-nandyala/azure-cli/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
   name "Azure CLI"
   desc "Microsoft Azure CLI 2.0"
   homepage "https://docs.microsoft.com/cli/azure/overview"
