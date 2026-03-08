902f0908b88a3aab0076edfe7ee2f18976543614
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2026-03-09 10:52:22 +1100

    updated tar file path

--- Casks/a/azure-cli.rb
+++ Casks/a/azure-cli.rb
@@ -6,7 +6,7 @@ cask "azure-cli" do
   sha256 arm:   "be1832758464d2f22ef0f4efcc5ee4155d253f12ddeb244b2dcca913992df987",
          intel: "862be75742e3dc2e92bee5ae68a7a67a4bd028ada5e1a1fdf92297fe822be62e"
 
-  url "https://github.com/naga-nandyala/azure-cli/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
+  url "https://github.com/Azure/homebrew-azure-cli/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
   name "Azure CLI"
   desc "Microsoft Azure CLI 2.0"
   homepage "https://docs.microsoft.com/cli/azure/overview"
