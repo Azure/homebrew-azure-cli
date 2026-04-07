c9423d9a49dba0f3d2011c49a6dfa6e84b4ab059
Naga Nandyala <naga.s.p.nandyala@gmail.com>
2026-04-08 09:57:32 +1000

    Add azure-cli-preview.rb, revert azure-cli.rb to original

--- Casks/a/azure-cli.rb
+++ Casks/a/azure-cli.rb
@@ -1,12 +1,12 @@
 cask "azure-cli" do
   arch arm: "arm64", intel: "x86_64"
+  os macos: "macos"
 
   version "2.85.0"
-  sha256 arm:   "cbc6b206504091a1cf82a1fb6ae87365f758bf6fd2214d8014cdf32a7fd3a94c",
-         intel: "c1a9c345dd30dd304b175d4691899ee1cbc77be17fe8ea57c236bac2275ad480"
+  sha256 arm:   "e7c557933a53adb106865870a7a2123dc9c5e2c746cf91ac4ba014cebe294a7f",
+         intel: "efe89c8d7f405e666f376f23c524d1c0512b700bca72e36c7e846457190a36e6"
 
-  url "https://github.com/Azure/azure-cli/releases/download/azure-cli-#{version}/azure-cli-#{version}-macos-#{arch}.tar.gz",
-      verified: "github.com/Azure/azure-cli/"
+  url "https://github.com/Azure/homebrew-azure-cli/releases/download/azure-cli-#{version}/azure-cli-#{version}-#{os}-#{arch}.tar.gz"
   name "Azure CLI"
   desc "Microsoft Azure CLI 2.0"
   homepage "https://docs.microsoft.com/cli/azure/overview"
@@ -19,9 +19,9 @@ cask "azure-cli" do
   depends_on formula: "python@3.13"
 
   binary "bin/az"
+  zsh_completion "completions/zsh/_az"
   bash_completion "completions/bash/az"
   fish_completion "completions/fish/az.fish"
-  zsh_completion "completions/zsh/_az"
 
   zap trash: "~/.azure"
 end
