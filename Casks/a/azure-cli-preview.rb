78bab72efe2f21fa123946229bacc1a4d22dfb7d
Azure CLI Team <AzPyCLI@microsoft.com>
2026-07-07 04:31:04 +0000

    azure-cli-preview 2.88.0

--- Casks/a/azure-cli-preview.rb
+++ Casks/a/azure-cli-preview.rb
@@ -1,9 +1,9 @@
 cask "azure-cli-preview" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "2.87.0"
-  sha256 arm:   "b1f8b777a928427def444a8d35e345a305595dd8c694912ab20a6bfd212b2009",
-         intel: "9dc324e3e9a87ef76f2abc0d0fad2b54300b38769d199bbf58fc000a218cba0b"
+  version "2.88.0"
+  sha256 arm:   "f97865d9300522481e39cd016005fc2fe0a35d8a8900e9ddc77c6f75984ffea9",
+         intel: "b50714213c46f354ac176f92fc5d6162e87525b3de6c893b00bbfa2eadb5f6b2"
 
   url "https://github.com/Azure/azure-cli/releases/download/azure-cli-#{version}/azure-cli-#{version}-macos-#{arch}.tar.gz",
       verified: "github.com/Azure/azure-cli/"
@@ -16,7 +16,7 @@ cask "azure-cli-preview" do
     strategy :github_latest
   end
 
-  depends_on formula: "python@3.13"
+  depends_on formula: "python@3.14"
 
   binary "bin/az"
   bash_completion "completions/bash/az"
